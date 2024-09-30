3def517705a1d09aaaecacf431f54e18bf83cff4
saddamhossain <saddamhossain953@gmail.com>
2024-09-30 16:02:56 +0600

    cleanup

--- KjYesudasHindi/Startup.cs
+++ KjYesudasHindi/Startup.cs
@@ -2,16 +2,10 @@ using KjYesudasHindi.Data;
 using KjYesudasHindi.Interface;
 using KjYesudasHindi.Service;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KjYesudasHindi
 {
